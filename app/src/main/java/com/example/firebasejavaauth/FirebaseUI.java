d6e73a0a5693b58e6053a9ef3e7810a4e99aa9a8
Timz Owen <ktimothy@kabarak.ac.ke>
2019-08-09 10:20:43 -0700

    Create FirebaseUI.java
    Updated the Simple Firebase UI

---
+++ app/src/main/java/com/example/firebasejavaauth/FirebaseUI.java
@@ -0,0 +1,36 @@
+
+List<AuthUI.IdpConfig> providers = Arrays.asList(
+        new AuthUI.IdpConfig.EmailBuilder().build(),
+        new AuthUI.IdpConfig.PhoneBuilder().build(),
+        new AuthUI.IdpConfig.GoogleBuilder().build(),
+        new AuthUI.IdpConfig.FacebookBuilder().build(),
+        new AuthUI.IdpConfig.TwitterBuilder().build());
+
+//Launcher intent
+startActivityForResult(
+        AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+        RC_SIGN_IN);
+        
+        //OnActivityResult open when Auht is succesful
+        @Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == RC_SIGN_IN) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (resultCode == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+}
